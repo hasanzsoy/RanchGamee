afd5152a403d9d24388f6456efc90f09d9c4daec
Hasan ÖZSOY <hasanzsoy@outlook.com>
2025-09-28 21:44:44 +0300

    ıosınput

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine.Rendering;
 public class PlayerController : MonoBehaviour
 {
     public float runningSpeed;
+    float touchXDelta = 0;
+    float newX = 0;
+    public float xSpeed;
+    public float limitX;
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
-      Vector3 newPosition = new Vector3(transform.position.x,transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
-      transform.position = newPosition;
+
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            //Debug.Log(Input.GetTouch(0).deltaPosition.x / Screen.width);
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+        //else if (Input.GetMouseButton(0))
+        //{
+        //    touchXDelta = Input.GetAxis("Mouse X");
+        //}
+
+
+
+        newX = transform.position.x + xSpeed * touchXDelta * Time.deltaTime;
+        newX = Mathf.Clamp(newX, -limitX, limitX);
+
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
+        transform.position = newPosition;
     }
 }
